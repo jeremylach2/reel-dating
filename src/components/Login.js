30f233ad9717500489b034a573b0005c0344af1a
Jeremy Lachowicz <jersoccer2@gmail.com>
2021-04-14 15:52:22 -0400

    Setup Main App Comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,23 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
 const Login = props => {
-}
+    return <Text>Hi</Text>;
+};
 
 export default Login;
