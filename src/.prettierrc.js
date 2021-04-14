f065c4bbfde11c2a8720084dff30829e72e8c8e8
Bryan Pikaard <bryan.pikaardjr@outlook.com>
2021-04-14 16:10:10 -0400

    Update eslint

--- src/.prettierrc.js
+++ src/.prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+    trailingComma: "es5",
+    jsxBracketSameLine: true,
+    arrowParens: "avoid",
+    tabWidth: 4
 };
