0a0469fb691ce45985544e187b06afb3c3cbc091
Jeremy Lachowicz <jersoccer2@gmail.com>
2021-04-14 20:42:11 -0400

    Started Login Screen
    This can use a lot of work on the style department

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
-
-<<<<<<< HEAD
-import React, { Component } from 'react';
 import type { Node } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+
 import Login from "./components/Login";
 
 const App = props => {
   return (<Login />);
-=======
-const App = props => {
-    return (
-        <View>
-            <Text>Hello there!</Text>
-        </View>
-    );
->>>>>>> login
-};
-
+}
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import {
     SafeAreaView,
     ScrollView,
@@ -7,6 +8,8 @@ import {
     Text,
     useColorScheme,
     View,
+    Image,
+    TextInput,
 } from 'react-native';
 import {
     Colors,
@@ -17,7 +20,60 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const Login = props => {
-    return <Text>Hi</Text>;
+    const [text, onChangeText] = React.useState("");
+    return(
+        <View style={page.background}>
+            <View style={page.top_background}>
+                <Image style={page.logo} source={require('../assets/images/logo.png')} />
+            </View>
+            <View style={page.container}>
+                <Text style={page.text}>Username: </Text>
+                <TextInput 
+                    style={page.textIn}
+                    onChangeText={onChangeText}
+                    value={text}
+                    placeholder="Username..."
+                    placeholderTextColor= 'white'
+                />
+            </View>
+        </View>
+    );
 };
+
+const page = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#000000',
+    },
+    top_background: {
+        backgroundColor: '#343635',
+    },
+    logo: {
+        resizeMode: 'contain',
+        height: 120,
+        width: '100%',
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    text: {
+        flex: 2,
+        padding: 10,
+        marginTop: 15,
+        marginLeft: 70,
+        color: 'white',
+    },
+    textIn: {
+        flex: 2,
+        borderWidth: 1,
+        borderColor: 'white',
+        marginTop: 20,
+        marginRight: 160,
+        height: 30,
+        width: "100%",
+        fontSize: 10,
+    }
+});
 
 export default Login;
