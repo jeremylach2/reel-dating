d5c2b5152ff7abdbfe9e1bc5adaf90c0e037e4a2
Jeremy Lachowicz <jersoccer2@gmail.com>
2021-04-15 15:03:36 -0400

    Login Screen Front End

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,13 @@ import {
     StyleSheet,
     Text,
     useColorScheme,
+    ImageBackground,
     View,
     Image,
     TextInput,
+    AppRegistry,
+    TouchableHighlight,
+    TouchableOpacity,
 } from 'react-native';
 import {
     Colors,
@@ -20,22 +24,51 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const Login = props => {
-    const [text, onChangeText] = React.useState("");
-    return(
-        <View style={page.background}>
-            <View style={page.top_background}>
-                <Image style={page.logo} source={require('../assets/images/logo.png')} />
-            </View>
-            <View style={page.container}>
-                <Text style={page.text}>Username: </Text>
-                <TextInput 
-                    style={page.textIn}
-                    onChangeText={onChangeText}
-                    value={text}
-                    placeholder="Username..."
-                    placeholderTextColor= 'white'
+    const [user, setUser] = React.useState("");
+    const [pass, setPass] = React.useState("");
+    const forgot = () => {
+
+    }
+
+    return (
+        <View style={page.container}>
+            <ImageBackground
+                source={require("../assets/images/background-login.jpg")}
+                style={page.background}>
+                <Image
+                    style={page.logo}
+                    source={require("../assets/images/logo.png")}
                 />
-            </View>
+                <View style={page.box}>
+                    <View style={page.form}>
+                        <Text style={page.title}>Login</Text>
+                        <View style={page.form_content}>
+                            <TextInput
+                                style={page.login}
+                                onChangeText={text => setUser(text)}
+                                value={user}
+                                placeholder="Username..."
+                                placeholderTextColor="black"
+                            />
+                            <TextInput
+                                style={page.login}
+                                onChangeText={text => setPass(text)}
+                                value={pass}
+                                placeholder="Password..."
+                                placeholderTextColor="black"
+                            />
+                            <TouchableOpacity>
+                                <View style={page.login_button}>
+                                    <Text style={page.login_text}>Login</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                        <TouchableOpacity onPress={() => forgot}>
+                            <Text style={page.forgot}>Forgot My Password </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ImageBackground>
         </View>
     );
 };
@@ -43,37 +76,73 @@ const Login = props => {
 const page = StyleSheet.create({
     background: {
         flex: 1,
-        backgroundColor: '#000000',
     },
-    top_background: {
-        backgroundColor: '#343635',
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
     },
     logo: {
-        resizeMode: 'contain',
-        height: 120,
-        width: '100%',
+        resizeMode: "contain",
+        height: 150,
+        width: "100%",
     },
     container: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: "row",
     },
-    text: {
-        flex: 2,
-        padding: 10,
-        marginTop: 15,
-        marginLeft: 70,
-        color: 'white',
+    box: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignContent: "center",
+
     },
-    textIn: {
-        flex: 2,
-        borderWidth: 1,
-        borderColor: 'white',
-        marginTop: 20,
-        marginRight: 160,
-        height: 30,
+    title: {
+        fontWeight: "bold",
+        textAlign: "center",
+        color: "black",
+        fontSize: 25,
+    },
+    form: {
+        backgroundColor: "white",
+        padding: "5%",
+        paddingBottom: "1%",
+        marginTop: "1%",
+        borderRadius: 8,
+        width: "70%",
+        flexWrap: "wrap",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+    form_content: {
+        display: "flex",
+        flexWrap: "wrap",
+        flexDirection: "column",
         width: "100%",
-        fontSize: 10,
-    }
+    },
+    login: {
+        backgroundColor: "gray",
+        borderRadius: 4,
+        padding: "3%",
+        marginBottom: "2%",
+        width: "100%",
+    },
+    login_button: {
+        backgroundColor: "#268CCB",
+        borderRadius: 4,
+        padding: "4%",
+        marginBottom: "4%",
+        width: "100%",
+    },
+    login_text: {
+        color: "white",
+        textAlign: "center",
+    },
+    forgot: {
+        color: "black",
+        opacity: 0.6,
+    },
 });
 
 export default Login;
