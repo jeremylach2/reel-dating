bc9b015a969d2454b03ce436fb38a17c96df3657
Jeremy Lachowicz <jersoccer2@gmail.com>
2021-04-18 21:30:01 -0400

    Started Settings Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import type { Node } from 'react';
+
+import Login from "./components/Login";
+import Settings from "./components/Settings";
+
 
 const App = props => {
-    return (
-        <View>
-            <Text>Hello there!</Text>
-        </View>
-    );
+    return (<Settings />);
 };
 
 export default App;
