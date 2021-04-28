daeb85ac9dd8df240b2a2fd073ff30579e3af641
Bryan Pikaard <bryan.pikaardjr@outlook.com>
2021-04-28 10:11:51 -0400

    Auto stash before merge of "database" and "main"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text } from "react-native";
 
-import Login from "./components/Login";
-import MenuScreen from "./components/MenuScreen";
-import Settings from "./components/settings/Options";
-import Nav from "./components/Nav";
-import VideoCall from "./components/VideoCall"
+import { firebase } from '@react-native-firebase/database';
+
+const fb = firebase
+    .app()
+    .database("https://reel-dating-default-rtdb.firebaseio.com");
 
 const App = props => {
-    return (<VideoCall />);
+    const [user, setUser] = useState({});
+
+    fb.ref("/users/1")
+        .once("value")
+        .then(v => setUser(v.val()));
+
+    return (
+        <Text>
+            {user && user.name
+                ? `${user.name.first} ${user.name.last}`
+                : "No user."}
+        </Text>
+    );
 };
 
 export default App;
